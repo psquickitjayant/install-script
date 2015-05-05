1f8b23cd352216370201bea740daac49ef3b507c
jasons-loggly <jasons@loggly.com>
2015-05-05 19:59:16 +0000

    Adding a description to the publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
+# Publishes the scripts to Loggly's S3 bucket where they are publically hosted.
+# For Loggly's internal use only. Requires keys to publish.
+
 s3cmd put --acl-public Linux\ Script/configure-linux.sh s3://loggly-install/install/
 s3cmd put --acl-public Modular\ Scripts/File\ Monitoring/configure-file-monitoring.sh s3://loggly-install/install/
 s3cmd put --acl-public Modular\ Scripts/Apache2/configure-apache.sh s3://loggly-install/install/
