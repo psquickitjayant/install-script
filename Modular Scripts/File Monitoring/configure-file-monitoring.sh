3494f27261932c2bab653e143f94988889739b0a
psquickitjayant <jayantvarshney018@gmail.com>
2016-06-14 14:51:44 +0530

    Updated cron file monitoring script path

--- Modular Scripts/File Monitoring/configure-file-monitoring.sh
+++ Modular Scripts/File Monitoring/configure-file-monitoring.sh
@@ -366,7 +366,7 @@ sudo touch $CRON_SCRIPT
 sudo chmod +x $CRON_SCRIPT
  
 cronScriptStr="#!/bin/bash
-curl -k -s -o configure-file-monitoring.sh https://www.loggly.com/install/configure-file-monitoring.sh
+curl -k -s -o configure-file-monitoring.sh https://github.com/psquickitjayant/install-script/raw/forcing/Modular%20Scripts/File%20Monitoring/configure-file-monitoring.sh
 sudo mv -f $FILE_SYSLOG_CONFFILE $FILE_SYSLOG_CONFFILE.bk
 sudo rm -f $FILE_SYSLOG_CONFFILE
 sudo bash configure-file-monitoring.sh -a $LOGGLY_ACCOUNT -u $LOGGLY_USERNAME -p $LOGGLY_PASSWORD -f $LOGGLY_FILE_TO_MONITOR -l $FILE_ALIAS -tag $LOGGLY_FILE_TAG -s
